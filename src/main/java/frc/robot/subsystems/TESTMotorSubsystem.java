18feab2bb36edb1d9252548b09fdec779364e7ac
Alex-Tsekhansky <atsekhan@gmail.com>
2022-02-14 22:20:16 -0500

    more changes for test

--- src/main/java/frc/robot/subsystems/TESTMotorSubsystem.java
+++ src/main/java/frc/robot/subsystems/TESTMotorSubsystem.java
@@ -37,6 +37,87 @@ public class TESTMotorSubsystem extends SubsystemBase {
 
   }
 
+  public void configurePanMotorControllerForPosition() {
+
+    // Reset Hardware - ex
+    testMotorController.configFactoryDefault();
+
+    // Configure the encoders for PID control
+    //panMotorController.configSelectedFeedbackSensor(FeedbackDevice.PulseWidthEncodedPosition, Constants.ShooterConstants.PID_PAN,
+    //  Constants.ShooterConstants.configureTimeoutMs);
+
+		/* Config the sensor used for Primary PID and sensor direction  - ex */
+    testMotorController.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 
+      Constants.ShooterConstants.PID_PAN,
+      Constants.ShooterConstants.configureTimeoutMs);
+
+  	/* Ensure sensor is positive when output is positive - ex */
+    testMotorController.setSensorPhase(Constants.ShooterConstants.SensorPhase);
+
+    		/**
+		 * Set based on what direction you want forward/positive to be.
+		 * This does not affect sensor phase. - ex 
+		 */ 
+		testMotorController.setInverted(Constants.ShooterConstants.MotorInvert);
+
+    /* Configure motor neutral deadband */
+    testMotorController.configNeutralDeadband(Constants.ShooterConstants.NeutralDeadband, Constants.ShooterConstants.configureTimeoutMs);
+
+    /**
+     * Max out the peak output (for all modes). However you can limit the output of
+     * a given PID object with configClosedLoopPeakOutput().
+     */
+
+    testMotorController.configPeakOutputForward(Constants.ShooterConstants.PeakOutput, Constants.ShooterConstants.configureTimeoutMs);
+    testMotorController.configPeakOutputReverse(Constants.ShooterConstants.PeakOutput*(-1), Constants.ShooterConstants.configureTimeoutMs);
+    testMotorController.configNominalOutputForward(0, Constants.ShooterConstants.configureTimeoutMs);
+    testMotorController.configNominalOutputReverse(0, Constants.ShooterConstants.configureTimeoutMs);
+
+		/**
+		 * Config the allowable closed-loop error, Closed-Loop output will be
+		 * neutral within this range. See Table in Section 17.2.1 for native
+		 * units per rotation. - ex
+		 */
+		testMotorController.configAllowableClosedloopError(Constants.ShooterConstants.SLOT_0,
+                                      Constants.ShooterConstants.panDefaultAcceptableError,
+                                      Constants.ShooterConstants.configureTimeoutMs);
+
+		/* Config Position Closed Loop gains in slot0, tsypically kF stays zero. */
+
+    /* FPID Gains for pan motor */
+
+    testMotorController.config_kP(Constants.ShooterConstants.SLOT_0, Constants.ShooterConstants.P_PAN, Constants.ShooterConstants.configureTimeoutMs);
+    testMotorController.config_kI(Constants.ShooterConstants.SLOT_0, Constants.ShooterConstants.I_PAN, Constants.ShooterConstants.configureTimeoutMs);
+    testMotorController.config_kD(Constants.ShooterConstants.SLOT_0, Constants.ShooterConstants.D_PAN, Constants.ShooterConstants.configureTimeoutMs);
+    testMotorController.config_kF(Constants.ShooterConstants.SLOT_0, Constants.ShooterConstants.F_PAN, Constants.ShooterConstants.configureTimeoutMs);
+
+		/**
+		 * Grab the 360 degree position of the MagEncoder's absolute
+		 * position, and intitally set the relative sensor to match. - ex
+		 */
+		int absolutePosition = testMotorController.getSensorCollection().getPulseWidthPosition();
+
+		/* Mask out overflows, keep bottom 12 bits */
+		absolutePosition &= 0xFFF;
+		if (Constants.ShooterConstants.SensorPhase) { absolutePosition *= -1; }
+		if (Constants.ShooterConstants.MotorInvert) { absolutePosition *= -1; }
+		
+		/* Set the quadrature (relative) sensor to match absolute */
+		testMotorController.setSelectedSensorPosition(absolutePosition, Constants.ShooterConstants.SLOT_0, Constants.ShooterConstants.configureTimeoutMs);
+
+    /**
+     * 1ms per loop. PID loop can be slowed down if need be. For example, - if
+     * sensor updates are too slow - sensor deltas are very small per update, so
+     * derivative error never gets large enough to be useful. - sensor movement is
+     * very slow causing the derivative error to be near zero.
+     */
+
+    /*
+    testMotorController.configClosedLoopPeriod(1, Constants.ShooterConstants.closedLoopPeriodMs, Constants.ShooterConstants.configureTimeoutMs);
+    */
+
+  } // End configuretestMotorControllerForPosition
+
   public void zeroTestEncoders() {
     testMotorController.setSelectedSensorPosition(0);
   }
