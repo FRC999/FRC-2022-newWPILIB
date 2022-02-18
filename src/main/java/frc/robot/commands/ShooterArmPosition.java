fe3c90a2630fcbc3a7f44dccc53b92c9239b5a80
Alex Tsekhansky <atsekhan@gmail.com>
2022-02-17 21:24:48 -0500

    remove unused imports

--- src/main/java/frc/robot/commands/ShooterArmPosition.java
+++ src/main/java/frc/robot/commands/ShooterArmPosition.java
@@ -6,7 +6,6 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.RobotContainer;
-import frc.robot.subsystems.ShooterSubsystem;
 
 public class ShooterArmPosition extends CommandBase {
   /** Creates a new ShooterArmPosition. */
