d343a6a81f013c3d6ee4dc26e8e0dd68a2ba1928
Alex Tsekhansky <atsekhan@gmail.com>
2022-02-22 13:00:20 -0500

    Separate command for one-button shooter

---
+++ src/main/java/frc/robot/commands/ShooterOneButtonShot.java
@@ -0,0 +1,33 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.RobotContainer;
+
+
+// This works fine s inline command as well, but we want to make this group used in Autonomous sequences as well as Teleop
+
+public class ShooterOneButtonShot extends SequentialCommandGroup {
+  /** Creates a new ShooterOneButtonShot. */
+  public ShooterOneButtonShot() {
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+    addCommands(
+      deadline (
+        sequence(
+          new WaitCommand(1),
+          new InstantCommand(RobotContainer.shooterSubsystem::extendPlunger),
+          new WaitCommand(1),
+          new InstantCommand(RobotContainer.shooterSubsystem::retractPlunger),
+          new InstantCommand(RobotContainer.shooterSubsystem::stopShooterWheelMotor)
+        ), // end sequence
+        new InstantCommand(RobotContainer.shooterSubsystem::startShooterWheelMotor,RobotContainer.shooterSubsystem)
+      ) //end deadline
+    );
+  }
+}
