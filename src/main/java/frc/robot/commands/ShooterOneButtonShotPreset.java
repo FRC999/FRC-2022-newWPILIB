d0e34061b6d9b38910876a8d23fea97464d2d5a7
Alex Tsekhansky <atsekhan@gmail.com>
2022-03-14 18:20:02 -0400

    modify shooter preset routine - start shooter wheels early

--- src/main/java/frc/robot/commands/ShooterOneButtonShotPreset.java
+++ src/main/java/frc/robot/commands/ShooterOneButtonShotPreset.java
@@ -17,22 +17,19 @@ public class ShooterOneButtonShotPreset extends SequentialCommandGroup {
   public ShooterOneButtonShotPreset(int distance, int goal) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
+    addRequirements(RobotContainer.shooterSubsystem); // interrupt other shooter commands if needed
     addCommands(
       sequence(
-        new InstantCommand(() -> RobotContainer.shooterSubsystem.setShootingSolution(distance,goal),RobotContainer.shooterSubsystem),
+        new InstantCommand(() -> RobotContainer.shooterSubsystem.setShootingSolution(distance,goal)),
+        // start shooter motor as soon as we know the shooting solution speed
+        new InstantCommand(() -> RobotContainer.shooterSubsystem.startShooterWheelMotor((RobotContainer.shooterSubsystem.getShootingSolution())[1])),
         new InstantCommand(() -> RobotContainer.shooterSubsystem.tiltShooterArm( (RobotContainer.shooterSubsystem.getShootingSolution())[0])),
-        new WaitCommand(1),
-        deadline (
-          sequence(
-            new WaitCommand(1),
-            new InstantCommand(RobotContainer.shooterSubsystem::extendPlunger),
-            new WaitCommand(0.2),
-            new InstantCommand(RobotContainer.shooterSubsystem::retractPlunger),
-            new InstantCommand(RobotContainer.shooterSubsystem::stopShooterWheelMotor)
-          ), // end sequence
-          new InstantCommand(() -> RobotContainer.shooterSubsystem.startShooterWheelMotor( (RobotContainer.shooterSubsystem.getShootingSolution())[1]))
-        ) //end deadline
-      )
+        new WaitCommand(1), // TODO - check the wheel speed, adjust this time to make sure the shooter wheel gets up to speed
+        new InstantCommand(RobotContainer.shooterSubsystem::extendPlunger),
+        new WaitCommand(0.2),
+        new InstantCommand(RobotContainer.shooterSubsystem::retractPlunger),
+        new InstantCommand(RobotContainer.shooterSubsystem::stopShooterWheelMotor)
+      ) // not bringing it down automatically, in case we have another ball in the hopper
     );
   }
 }
