6053209c90db9778493a7b4cf59f99507aa538fb
Alex Tsekhansky <atsekhan@gmail.com>
2022-03-22 21:30:15 -0400

    T18

--- src/main/java/frc/robot/commands/TESTMotionMagic1motor.java
+++ src/main/java/frc/robot/commands/TESTMotionMagic1motor.java
@@ -5,6 +5,7 @@
 package frc.robot.commands;
 
 import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
+import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
@@ -19,7 +20,12 @@ public class TESTMotionMagic1motor extends CommandBase {
   public static final int kSlotIdx = 0;
 	public static final int kPIDLoopIdx = 0;
 	public static final int kTimeoutMs = 30;
-  static final Gains kGains = new Gains(0.2, 0.0, 0.0, 0.2, 0, 1.0);
+  double kP = 0.2;
+  double kI = 0.0;
+  double kD = 0.0;
+  double kF = 0.2;
+  int kIzone = 0;
+  double kPeakOutput = 1.0;
 
 
   public TESTMotionMagic1motor(WPI_TalonFX cTalon) {
@@ -40,51 +46,56 @@ public class TESTMotionMagic1motor extends CommandBase {
     _talon.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, kPIDLoopIdx,
     kTimeoutMs);
 
-/* set deadband to super small 0.001 (0.1 %).
-  The default deadband is 0.04 (4 %) */
-_talon.configNeutralDeadband(0.001, kTimeoutMs);
+    /*
+     * set deadband to super small 0.001 (0.1 %).
+     * The default deadband is 0.04 (4 %)
+     */
+    _talon.configNeutralDeadband(0.001, kTimeoutMs);
 
-/**
- * Configure Talon FX Output and Sensor direction accordingly Invert Motor to
- * have green LEDs when driving Talon Forward / Requesting Postiive Output Phase
- * sensor to have positive increment when driving Talon Forward (Green LED)
- */
-_talon.setSensorPhase(false);
-_talon.setInverted(false);
+    /**
+     * Configure Talon FX Output and Sensor direction accordingly Invert Motor to
+     * have green LEDs when driving Talon Forward / Requesting Postiive Output Phase
+     * sensor to have positive increment when driving Talon Forward (Green LED)
+     */
+    _talon.setSensorPhase(false);
+    _talon.setInverted(false);
 
-/*
- * Talon FX does not need sensor phase set for its integrated sensor
- * This is because it will always be correct if the selected feedback device is integrated sensor (default value)
- * and the user calls getSelectedSensor* to get the sensor's position/velocity.
- * 
- * https://phoenix-documentation.readthedocs.io/en/latest/ch14_MCSensor.html#sensor-phase
- */
+    /*
+     * Talon FX does not need sensor phase set for its integrated sensor
+     * This is because it will always be correct if the selected feedback device is
+     * integrated sensor (default value)
+     * and the user calls getSelectedSensor* to get the sensor's position/velocity.
+     * 
+     * https://phoenix-documentation.readthedocs.io/en/latest/ch14_MCSensor.html#
+     * sensor-phase
+     */
     // _talon.setSensorPhase(true);
 
-/* Set relevant frame periods to be at least as fast as periodic rate */
-_talon.setStatusFramePeriod(StatusFrameEnhanced.Status_13_Base_PIDF0, 10, kTimeoutMs);
-_talon.setStatusFramePeriod(StatusFrameEnhanced.Status_10_MotionMagic, 10, kTimeoutMs);
+    /* Set relevant frame periods to be at least as fast as periodic rate */
+    _talon.setStatusFramePeriod(StatusFrameEnhanced.Status_13_Base_PIDF0, 10, kTimeoutMs);
+    _talon.setStatusFramePeriod(StatusFrameEnhanced.Status_10_MotionMagic, 10, kTimeoutMs);
 
-/* Set the peak and nominal outputs */
-_talon.configNominalOutputForward(0, kTimeoutMs);
-_talon.configNominalOutputReverse(0, kTimeoutMs);
-_talon.configPeakOutputForward(1, kTimeoutMs);
-_talon.configPeakOutputReverse(-1, kTimeoutMs);
+    /* Set the peak and nominal outputs */
+    _talon.configNominalOutputForward(0, kTimeoutMs);
+    _talon.configNominalOutputReverse(0, kTimeoutMs);
+    _talon.configPeakOutputForward(1, kTimeoutMs);
+    _talon.configPeakOutputReverse(-1, kTimeoutMs);
 
-/* Set Motion Magic gains in slot0 - see documentation */
-_talon.selectProfileSlot(kSlotIdx, kPIDLoopIdx);
-_talon.config_kF(kSlotIdx, kGains.kF, kTimeoutMs);
-_talon.config_kP(kSlotIdx, kGains.kP, kTimeoutMs);
-_talon.config_kI(kSlotIdx, kGains.kI, kTimeoutMs);
-_talon.config_kD(kSlotIdx, kGains.kD, kTimeoutMs);
+    /* Set Motion Magic gains in slot0 - see documentation */
+    _talon.selectProfileSlot(kSlotIdx, kPIDLoopIdx);
+    _talon.config_kF(kSlotIdx, kF, kTimeoutMs);
+    _talon.config_kP(kSlotIdx, kP, kTimeoutMs);
+    _talon.config_kI(kSlotIdx, kI, kTimeoutMs);
+    _talon.config_kD(kSlotIdx, kD, kTimeoutMs);
 
-/* Set acceleration and vcruise velocity - see documentation */
-_talon.configMotionCruiseVelocity(15000, kTimeoutMs);
-_talon.configMotionAcceleration(6000, kTimeoutMs);
+    /* Set acceleration and vcruise velocity - see documentation */
+    _talon.configMotionCruiseVelocity(15000, kTimeoutMs);
+    _talon.configMotionAcceleration(6000, kTimeoutMs);
 
-/* Zero the sensor once on robot boot up */
-_talon.setSelectedSensorPosition(0, kPIDLoopIdx, kTimeoutMs);
+    /* Zero the sensor once on robot boot up */
+    _talon.setSelectedSensorPosition(0, kPIDLoopIdx, kTimeoutMs);
 
+    _talon.set(TalonFXControlMode.MotionMagic, 10000);
 
   }
 
