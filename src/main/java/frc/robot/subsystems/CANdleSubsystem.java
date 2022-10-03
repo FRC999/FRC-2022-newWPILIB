80290faa4a8d2605ad71b0a7596d736fb32cdd19
Coypirus <coypirus@gmail.com>
2022-10-03 19:56:03 -0400

    LED button 2

--- src/main/java/frc/robot/subsystems/CANdleSubsystem.java
+++ src/main/java/frc/robot/subsystems/CANdleSubsystem.java
@@ -81,4 +81,44 @@ public class CANdleSubsystem extends SubsystemBase {
     candle.setLEDs(0,0,0);
     candle.modulateVBatOutput(0);
   }
+
+  public void changeAnimation(AnimationTypes toChange) {
+    //m_currentAnimation = toChange;
+    
+    switch(toChange)
+    {
+        case ColorFlow:
+            m_toAnimate = new ColorFlowAnimation(128, 20, 70, 0, 0.7, LedCount, Direction.Forward);
+            break;
+        case Fire:
+            m_toAnimate = new FireAnimation(0.5, 0.7, LedCount, 0.7, 0.5);
+            break;
+        case Larson:
+            m_toAnimate = new LarsonAnimation(0, 255, 46, 0, 1, LedCount, BounceMode.Front, 3);
+            break;
+        case Rainbow:
+            m_toAnimate = new RainbowAnimation(1, 0.1, LedCount);
+            break;
+        case RgbFade:
+            m_toAnimate = new RgbFadeAnimation(0.7, 0.4, LedCount);
+            break;
+        case SingleFade:
+            m_toAnimate = new SingleFadeAnimation(50, 2, 200, 0, 0.5, LedCount);
+            break;
+        case Strobe:
+            m_toAnimate = new StrobeAnimation(240, 10, 180, 0, 98.0 / 256.0, LedCount);
+            break;
+        case Twinkle:
+            m_toAnimate = new TwinkleAnimation(30, 70, 60, 0, 0.4, LedCount, TwinklePercent.Percent6);
+            break;
+        case TwinkleOff:
+            m_toAnimate = new TwinkleOffAnimation(70, 90, 175, 0, 0.8, LedCount, TwinkleOffPercent.Percent100);
+            break;
+        case SetAll:
+            m_toAnimate = null;
+            break;
+    }
+    //System.out.println("Changed to " + m_currentAnimation.toString());
+  }
+
 }
